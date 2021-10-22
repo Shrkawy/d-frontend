f44e6f544415a23dd8c6c051384afcc7158982c4
Sharkawy <mahmoud.alshrkawy@gmail.com>
2021-10-22 15:52:05 +0200

    Add Announcement component

---
+++ src/components/Announcement.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Announcement = () => {
+  return <Container>Lorem ipsum dolor sit amet.</Container>;
+};
+
+const Container = styled.div`
+  height: 1.875rem;
+  background-color: teal;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 300;
+`;
+
+export default Announcement;
